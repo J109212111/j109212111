0b4221972fe5521bbca22f5d73f938652e2e8ae5
j109212111 <j109212111@nkust.edu.tw>
2021-01-06 23:16:34 +0800

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,14 +8,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>邱安</title>
+    <title>林偉挺</title>
   </head>
   <body>
   <div class="container">
-    <h1>邱安的期末作業</h1>
+    <h1>林偉挺的期末報告網</h1>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">J109212109</a>
+    <a class="navbar-brand" href="#">J109212111</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
